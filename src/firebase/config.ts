d34d80b4ee1b6efc6936d885ca21a673a0a0f67a
cybearz <goryashin.sanya@gmail.com>
2023-07-01 14:49:58 +0300

    Firebase init

---
+++ src/firebase/config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore, serverTimestamp } from "firebase/firestore"
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAwBX7NoH46sYqhhjqePgCZvNaoE6Rt9wE",
+	authDomain: "echo-verse.firebaseapp.com",
+	projectId: "echo-verse",
+	storageBucket: "echo-verse.appspot.com",
+	messagingSenderId: "609593986796",
+	appId: "1:609593986796:web:d16dfab16aa3c319c6dfc9",
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+export { app, auth, db, serverTimestamp }
